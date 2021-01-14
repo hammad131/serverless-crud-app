a2dbf47de6693b885681fa13aacd4cce3c837b58
hammad131 <74175920+hammad131@users.noreply.github.com>
2021-01-14 19:37:11 +0500

    updated ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import ContentEditable from './components/ContentEditable'
-import AppHeader from './components/AppHeader'
+import { AppHeader } from './components/AppHeader'
 import SettingsMenu from './components/SettingsMenu'
 import SettingsIcon from './components/SettingsIcon'
 import analytics from './utils/analytics'
--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,12 +1,38 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 
-const AppHeader = (props) => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+   
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: 'center'
+  },
+}));
+
+export default function AppHeader() {
+  const classes = useStyles();
+
   return (
-    <div>
-      Todo App By MHR
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          
+          <Typography variant="h6" className={classes.title}>
+            Todo App By MHR
+          </Typography>
+          
+        </Toolbar>
+      </AppBar>
     </div>
-  )
+  );
 }
-
-export default AppHeader
